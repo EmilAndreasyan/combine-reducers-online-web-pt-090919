1b724f4cdffe3e82717b6ef3ea8979bcd65d2c47
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-09 05:31:09 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -35,39 +35,6 @@ function authorsReducer(state = [], action) {
     default:
       return state;
   }
-}import {combineReducers} from 'redux';
-
-const rootReducer = combineReducers({
-  authors: authorsReducer,
-  books: bookReducer
-});
-
-export default rootReducer;
-
-function bookReducer(state = [], action){
-  let idx;
-  switch(action.type){
-    case "ADD_BOOK":
-      return [...state, action.book];
-    case "REMOVE_BOOK":
-      idx = state.findIndex(book => book.id === action.id)
-      return [...state.slice(0, idx), ...[state.slice(idx + 1)]]
-    default:
-      return state;
-  }
-}
-
-function authorsReducer(state = [], action){
-  let idx;
-  switch(action.type){
-    case "ADD_AUTHOR":
-      return [...state, action.author];
-    case "REMOVE_AUTHOR":
-      idx = state.findIndex(author => author.id === action.id)
-      return [...state.slice(0, idx), ...state.slice(idx + 1)];
-      default:
-      return state
-  }
 }
 
 // export default function bookApp(
